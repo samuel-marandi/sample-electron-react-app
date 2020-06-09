362083a7b501bcfb772d1fd11cff211a251336ed
Samuel Marandi <samuel@91social.com>
2020-06-09 17:58:20 +0530

    new updates

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+autoUpdater.logger = require("electron-log")
+autoUpdater.autoDownload = true 
+const logger = require('electron-log');
 
 let mainWindow;
 
@@ -22,7 +25,44 @@ function createWindow () {
     });
 }
 
-app.on('ready', () => {
+app.on('ready', async () => {
+    //try to update!
+    try {
+        const info = await autoUpdater.checkForUpdatesAndNotify();
+        logger.info('checkForUpdatesAndNotify');
+        logger.info(JSON.stringify(info));
+        
+
+        autoUpdater.on('update-downloaded', info => {
+            const quitAndInstalled = autoUpdater.quitAndInstall();
+            logger.info('quitAndInstalled');
+            logger.info(quitAndInstalled);
+        });
+
+        autoUpdater.on('update-available', arg => {
+            logger.info('update-available');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('update-not-available', arg => {
+            logger.info('update-not-available');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('download-progress', arg => {
+            logger.info('download-progress');
+            logger.info(arg);
+        });
+
+        autoUpdater.on('error', error => {
+            logger.info('error');
+            logger.info(error.message);
+            logger.info(error.stack);
+        });
+    } catch (error) {
+        logger.info('autoupdate failed');
+    }
+
     createWindow();
 });
 
@@ -42,13 +82,15 @@ ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-});
+// autoUpdater.on('update-available', () => {
+//     logger.info("Update available")
+//     mainWindow.webContents.send('update_available');
+// });
 
-autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-});
+// autoUpdater.on('update-downloaded', () => {
+//     logger.info("Update downloaded")
+//     mainWindow.webContents.send('update_downloaded');
+// });
 
 ipcMain.on('restart_app', () => {
     autoUpdater.quitAndInstall();
